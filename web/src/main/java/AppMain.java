cac74d32011f604e7828257472e1dc2bbe6b3a15
yiwen.pan <yiwen.pan@ele.me>
2016-02-26 10:25:57 +0800

    springboot集成mybatis

---
+++ web/src/main/java/AppMain.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by pan on 16/2/25.
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "paner.controller,paner.das.mapper")
+@ImportResource("classpath:mybatis-context.xml")
+public class AppMain {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
---
+++ web/src/main/java/paner/controller/Example.java
@@ -0,0 +1,30 @@
+package paner.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import paner.das.mapper.UserMapper;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by pan on 16/2/25.
+ */
+
+@RestController
+public class Example {
+
+     Logger logger = LoggerFactory.getLogger(Example.class);
+
+    @Resource
+    private UserMapper userMapper;
+
+    @RequestMapping("/")
+    String home() {
+        logger.info("启动成功");
+        return userMapper.getUserUtpRole(33031695);
+    }
+}
